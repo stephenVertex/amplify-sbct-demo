d790ff134931ad056283718515a877584f64df24
Stephen Barr <stephen@vertexdev.com.au>
2024-09-05 01:43:37 -0700

    Changing datetime to UTC milliseconds for ease of everything

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -24,7 +24,7 @@ const schema = a.schema({
             estimated_time_mins: a.integer(), // Fields are optional by default, but you can make them required
             priority: a.integer(), // Lets assume 1 to 5, with 1 being the lowest
             tags: a.string().array(), // This is the proper syntax for making a field into an array,
-	    scheduled_date: a.datetime(), 
+	    scheduled_date_millis: a.integer(), 
         })
         .authorization((allow) => [allow.publicApiKey()]),
 });
